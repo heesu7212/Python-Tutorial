5aadc3d0550823e83334fd8aae0ce41e1747232e
heesu7212 <yoonghethsiu@gmail.com>
2023-02-24 22:12:38 +0900

    Learning Variables in Python

---
+++ 6_variable.py
@@ -0,0 +1,12 @@
+price = 1000
+vat_rate = 0.1
+vat = price * vat_rate
+print(vat)
+
+print('hi, egoing .... bye, egoing.')
+
+name = 'sori'
+print('hi, '+name+' .... bye, '+name+'.')
+
+message= 'hi, '+name+' .... bye, '+name+'.'
+print(message)
