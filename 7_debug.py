6c56415ce0ddda0fbb7f625889e2b89864ee41ad
heesu7212 <yoonghethsiu@gmail.com>
2023-02-25 23:58:21 +0900

    Learning Debugging in Python

--- 7_debug.py
+++ 7_debug.py
@@ -1,12 +1,20 @@
+#example
 a = 1
 b = 2
 c = 3
 d = 4
+print(a)
+#3. The value is 1 here, hence its between point 2 and 3.
 e = 5
 f = 6
 g = 7
+print(a)
+#4. The value is still 1
 a = 8
+#5. This is the cause of error!
 h = 9
+print(a)
+#2. The value is still 8 here
 i = 8
 j = 7
 k = 6
@@ -15,7 +23,5 @@ m = 4
 n = 3
 o = 2
 p = 1
-
 print(a)
-print('hi', hi)
-print("hi")
+#1. The value came out as '8',instead of 1. Trying to debug the error.
